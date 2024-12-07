e13fbec8a5d1037a45d044cc8615e9486f741bba
Muhammad Shafique <shafiquecbl@gmail.com>
2024-12-07 23:36:23 +0500

    code updated

--- lib/view/screens/home/home.dart
+++ lib/view/screens/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:startup_repo/controller/theme_controller.dart';
 import 'package:startup_repo/utils/colors.dart';
 
 import 'package:startup_repo/utils/style.dart';
@@ -81,7 +83,7 @@ class HomeScreen extends StatelessWidget {
               'Label Small(${labelSmall(context).fontSize?.ceilToDouble()})',
               style: labelSmall(context),
             ),
-            SizedBox(height: 16.sp),
+            SizedBox(height: 32.sp),
             Row(
               children: [
                 Expanded(
@@ -101,9 +103,83 @@ class HomeScreen extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(height: 32.sp),
+            GetBuilder<ThemeController>(builder: (con) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ThemeModeWidget(
+                    text: 'system',
+                    themeMode: ThemeMode.system,
+                    selected: con.themeMode == ThemeMode.system,
+                  ),
+                  ThemeModeWidget(
+                    text: 'light',
+                    themeMode: ThemeMode.light,
+                    selected: con.themeMode == ThemeMode.light,
+                  ),
+                  ThemeModeWidget(
+                    text: 'dark',
+                    themeMode: ThemeMode.dark,
+                    selected: con.themeMode == ThemeMode.dark,
+                  ),
+                ],
+              );
+            }),
           ],
         ),
       ),
     );
+  }
+}
+
+class ThemeModeWidget extends StatelessWidget {
+  final String text;
+  final ThemeMode themeMode;
+  final bool selected;
+  const ThemeModeWidget({required this.text, required this.themeMode, required this.selected, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => ThemeController.find.setThemeMode(themeMode),
+      child: Column(
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            width: 60.sp,
+            height: 60.sp,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: selected ? primaryColor : Theme.of(context).hintColor,
+                width: 1.5.sp,
+              ),
+            ),
+            child: Icon(
+              icon,
+              size: 18.sp,
+              color: selected ? Theme.of(context).textTheme.bodyMedium?.color : Theme.of(context).hintColor,
+            ),
+          ),
+          SizedBox(height: 8.sp),
+          Text(
+            text,
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+        ],
+      ),
+    );
+  }
+
+  IconData get icon {
+    switch (themeMode) {
+      case ThemeMode.system:
+        return Iconsax.monitor_mobbile;
+      case ThemeMode.light:
+        return Iconsax.sun_1;
+      case ThemeMode.dark:
+        return Iconsax.moon;
+    }
   }
 }
